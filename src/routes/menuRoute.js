c59302e39757bcad7f718b2e3a429ec659b6d519
Ademola Hussain <demola.hussainin@gmail.com>
2020-07-24 23:50:28 +0100

    Enable a registered user to create a menu

--- src/routes/menuRoute.js
+++ src/routes/menuRoute.js
@@ -1,7 +1,7 @@
 import HelperMethods from '../helpers/helperMethods';
 import MenuController from '../controllers/menu';
 import Authorization from '../middleware/authorization';
-import { validateGetAMenu, validateEditMenu, } from '../middleware/menu';
+import { validateGetAMenu, validateEditMenu, validateCreateMenu, } from '../middleware/menu';
 
 const menuRoutes = app => {
   app.get(
@@ -21,6 +21,13 @@ const menuRoutes = app => {
     validateEditMenu(),
     HelperMethods.validateMiddleware,
     MenuController.editMenu,
+  );
+  app.post(
+    '/api/v1/menu',
+    Authorization.checkToken,
+    validateCreateMenu(),
+    HelperMethods.validateMiddleware,
+    MenuController.createMenu,
   );
 };
 
