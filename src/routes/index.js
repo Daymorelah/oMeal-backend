200c2d4411799c3c2246e134c662aa1d09bd58eb
Ademola Hussain <demola.hussainin@gmail.com>
2020-07-26 17:53:04 +0100

    Add a redirect on the index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ import orderRoute from './orderRoute';
  * @returns {object} - An object containing all routes
  */
 const routes = app => {
+  app.get('/', (req, res) => res.redirect('/api/v1/'))
   app.get('/api/v1/', (req, res) => {
     res.status(200).json({
       success: true,
